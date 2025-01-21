73e0c3f397d9dd022ad0a95945504470c084ab84
Noureddin El Adila <noureddineladila@gmail.com>
2025-01-21 19:10:27 +0100

    Create form's Components

---
+++ ProNeighbor/src/Components/Forms/user-registration/user-registration.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserRegistrationComponent } from './user-registration.component';
+
+describe('UserRegistrationComponent', () => {
+  let component: UserRegistrationComponent;
+  let fixture: ComponentFixture<UserRegistrationComponent>;
+  let compiled: HTMLElement;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UserRegistrationComponent],
+      imports: [UserRegistrationComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UserRegistrationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
