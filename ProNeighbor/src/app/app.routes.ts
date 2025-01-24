629a08672b439f963b3b53505c5196c007dda1c6
Noureddin El Adila <noureddineladila@gmail.com>
2025-01-24 17:12:37 +0100

    Add some routes

--- ProNeighbor/src/app/app.routes.ts
+++ ProNeighbor/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { UserRegistrationComponent } from '../Components/Forms/user-registration
 import { LoginComponent } from '../Components/Forms/login/login.component';
 import { HomeComponent } from '../Components/home/home.component';
 import { authGuard } from '../guards/auth.guard';
+import { ROLS } from '../enums/enum';
+import { UnauthorizedComponent } from '../Components/unauthorized/unauthorized.component';
+import { UserAreaComponent } from '../Components/user-area/user-area.component';
+import { ProviderAreaComponent } from '../Components/provider-area/provider-area.component';
 authGuard;
 export const routes: Routes = [
   {
@@ -22,5 +26,24 @@ export const routes: Routes = [
     path: 'home',
     component: HomeComponent,
     canActivate: [authGuard],
+    data: { roles: [ROLS.USER, ROLS.PROVIDER] },
+  },
+  {
+    path: 'unauthorized',
+    component: UnauthorizedComponent,
+    canActivate: [authGuard],
+    data: { roles: [ROLS.USER, ROLS.PROVIDER] },
+  },
+  {
+    path: 'userArea',
+    component: UserAreaComponent,
+    canActivate: [authGuard],
+    data: { roles: [ROLS.USER] },
+  },
+  {
+    path: 'providerArea',
+    component: ProviderAreaComponent,
+    canActivate: [authGuard],
+    data: { roles: [ROLS.PROVIDER] },
   },
 ];
