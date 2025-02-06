b13303a7aa9a1a5294f908e5fff72babfa905a5e
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-06 18:54:33 +0100

    Correct Environment routes

--- ProNeighbor/src/environments/environment.development.ts
+++ ProNeighbor/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  api_url_login: 'http://localhost:3000/auth/login',
-  api_url_register: 'http://localhost:3000/auth/register',
+  api_url_login: 'http://localhost:3000/api/auth/login',
+  api_url_register: 'http://localhost:3000/api/auth/register',
 };
