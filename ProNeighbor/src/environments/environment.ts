045d39e95b4b30550d5011becfc420333f1a4750
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-10 18:22:44 +0100

    Implementing - Should get user profile

--- ProNeighbor/src/environments/environment.ts
+++ ProNeighbor/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   api_url_login: 'http://localhost:3000/api/auth/login',
   api_url_register: 'http://localhost:3000/api/auth/register',
+  api_url_GetProfileInfo: 'http://localhost:3000/api/user/profile',
 };
--- ProNeighbor/src/services/users.service.ts
+++ ProNeighbor/src/services/users.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  constructor(private http: HttpClient) {}
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token'); // Adjust based on where you store the token
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+  }
 
-  constructor() { }
+  getUserProfile(): Observable<any> {
+    return this.http.get<any>(`${environment.api_url_GetProfileInfo}`, {
+      headers: this.getHeaders(),
+    });
+  }
 }
