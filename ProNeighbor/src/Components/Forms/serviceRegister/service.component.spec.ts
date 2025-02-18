c3eee43f9fb7dfe35a2a48e3e206943e7c820714
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-18 12:49:56 +0100

    TEST - should initialize categories on component load

--- ProNeighbor/src/Components/Forms/serviceRegister/service.component.spec.ts
+++ ProNeighbor/src/Components/Forms/serviceRegister/service.component.spec.ts
@@ -1,23 +1,61 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ServiceComponent } from './service.component';
+import { ServicesService } from '../../../services/services.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import {
+  Categories,
+  Category,
+  Service,
+} from '../../../Model/servicesProvider/service.model';
+
+// Mock Categories wrapped in Categories class
+const mockCategories: Category[] = [
+  new Category('Category 1'),
+  new Category('Category 2'),
+  new Category('Category 3'),
+];
+
+// Create mock service data
+const mockService = new Service(
+  'BFB2FDF9-07FF-4360-BE9E-222C2EAA4AED',
+  'Sample Service',
+  100,
+  'This is a description of the service',
+  'Sample Location',
+  new Date(),
+  'Category 1'
+);
 
 describe('ServiceComponent', () => {
   let component: ServiceComponent;
   let fixture: ComponentFixture<ServiceComponent>;
+  let servicesService: ServicesService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ServiceComponent]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule],
+      providers: [ServicesService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ServiceComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    servicesService = TestBed.inject(ServicesService);
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+  it('should initialize categories on component load', () => {
+    // Spy on the 'getAllCategories' method and mock the return value as an instance of Categories
+    spyOn(servicesService, 'getAllCategories').and.returnValue(
+      of(mockCategories as any)
+    );
+
+    // Call ngOnInit, which will trigger the service call
+    component.ngOnInit();
+
+    // Now we are comparing the Categories class
+    expect(component.categories?.Categories).toEqual(mockCategories);
   });
 });
