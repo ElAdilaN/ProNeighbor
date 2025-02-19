31b30ea552894301755de7cd90479263c217d8cd
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-19 11:16:52 +0100

    Add EndPoints in Environment

--- ProNeighbor/src/environments/environment.ts
+++ ProNeighbor/src/environments/environment.ts
@@ -8,4 +8,8 @@ export const environment = {
   api_url_GetAllServices: 'http://localhost:3000/api/services',
   api_url_GetAllServicesByProvider:
     'http://localhost:3000/api/services/ByProvider/',
+  api_url_GetAllReviewsForService: 'http://localhost:3000/api/reviews/service/',
+  api_url_GetAllReviewsForUser: 'http://localhost:3000/api/reviews/my-reviews',
+  api_url_UpdateReview: 'http://localhost:3000/api/reviews/',
+  api_url_DeleteReview: 'http://localhost:3000/api/reviews/',
 };
