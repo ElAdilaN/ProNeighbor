957c28b87ba6855959fe155ddca823c2284fafd9
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-19 09:32:25 +0100

    IMPLEMENTING - ServiceCardComponent - should display service details - should have correct "View Details" link

--- ProNeighbor/src/Components/AppServices/service-card/service-card.component.ts
+++ ProNeighbor/src/Components/AppServices/service-card/service-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Service } from '../../../Model/servicesProvider/service.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-service-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './service-card.component.html',
   styleUrl: './service-card.component.css',
 })
-export class ServiceCardComponent {}
+export class ServiceCardComponent {
+  @Input() service!: Service;
+}
