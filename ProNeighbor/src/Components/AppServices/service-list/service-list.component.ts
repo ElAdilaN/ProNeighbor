1139f8918ec2481c94f75572d1c396677bc16d5f
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-19 09:53:19 +0100

    IMPLEMENTING -  ServiceListComponent -  should call getAllServices() on init and load services - should handle error when getAllServices() fails - should show loading message while fetching services

--- ProNeighbor/src/Components/AppServices/service-list/service-list.component.ts
+++ ProNeighbor/src/Components/AppServices/service-list/service-list.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Service } from '../../../Model/servicesProvider/service.model';
+import { ServicesService } from '../../../services/services.service';
+import { ServiceCardComponent } from '../service-card/service-card.component';
+
 @Component({
   selector: 'app-service-list',
   standalone: true,
-  imports: [],
+  imports: [ServiceCardComponent],
   templateUrl: './service-list.component.html',
   styleUrl: './service-list.component.css',
 })
-export class ServiceListComponent {}
+export class ServiceListComponent implements OnInit {
+  services: Service[] = [];
+  isLoading = true;
+  errorMessage = '';
+
+  constructor(private serviceService: ServicesService) {}
+
+  ngOnInit(): void {
+    this.serviceService.getAllServices().subscribe({
+      next: (response: any) => {
+        this.services = response.services;
+        console.log(this.services);
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.errorMessage = 'Failed to load services';
+        this.isLoading = false;
+      },
+    });
+  }
+}
