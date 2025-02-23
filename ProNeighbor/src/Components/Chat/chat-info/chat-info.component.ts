7f114681faa45cd80d6e8844ee242e5d406fc15a
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-23 21:52:17 +0100

    IMPLEMENTING - ChatInfoComponent : should create component and load chat details on init   - should display chat details in the template

--- ProNeighbor/src/Components/Chat/chat-info/chat-info.component.ts
+++ ProNeighbor/src/Components/Chat/chat-info/chat-info.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ChatServiceService } from '../../../services/chat-service.service';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-chat-info',
   standalone: true,
@@ -6,4 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './chat-info.component.html',
   styleUrl: './chat-info.component.css',
 })
-export class ChatInfoComponent {}
+export class ChatInfoComponent {
+  chatDetails: any;
+  chatId?: string | null;
+  constructor(
+    private chatService: ChatServiceService,
+    private route: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    this.chatId = this.route.snapshot.paramMap.get('chatId');
+    if (this.chatId) {
+      this.getChatDetails(this.chatId);
+    }
+  }
+  getChatDetails(chatId: string): void {
+    this.chatService.getChatInfo(chatId).subscribe((details) => {
+      this.chatDetails = details;
+    });
+  }
+}
