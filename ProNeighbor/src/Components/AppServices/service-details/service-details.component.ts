a93d3a1e46c922b8f1c6e561a950875e3b42f60e
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-23 21:09:15 +0100

    IMPLEMENTING -  ServiceDetailsComponent :  should load service details on init - should handle error when getServiceById() fails -  should show loading message while fetching service details

--- ProNeighbor/src/Components/AppServices/service-details/service-details.component.ts
+++ ProNeighbor/src/Components/AppServices/service-details/service-details.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
-import { ReviewListComponent } from '../../Reviews/review-list/review-list.component';
+import { Component } from "@angular/core";
+import { Service } from "../../../Model/servicesProvider/service.model";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ServicesService } from "../../../services/services.service";
+import { ReviewListComponent } from "../../Reviews/review-list/review-list.component";
 
 @Component({
-  selector: 'app-service-details',
+  selector: "app-service-details",
   standalone: true,
   imports: [ReviewListComponent],
-  templateUrl: './service-details.component.html',
-  styleUrl: './service-details.component.css',
+  templateUrl: "./service-details.component.html",
+  styleUrl: "./service-details.component.css",
 })
 export class ServiceDetailsComponent {
+  isLoading = true;
+  errorMessage = "";
+  serviceId: string | null = null;
+  service: Service | null = null;
 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private servicesService: ServicesService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.serviceId = params.get("id");
+      if (this.serviceId) {
+        this.loadServiceDetails(this.serviceId);
+      }
+    });
+  }
+
+  loadServiceDetails(id: string): void {
+    this.servicesService.getServiceById(id).subscribe({
+      next: (data: any) => {
+        this.service = data.service;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.errorMessage = "Failed to load service details.";
+        this.isLoading = false;
+      },
+    });
+  }
 }
