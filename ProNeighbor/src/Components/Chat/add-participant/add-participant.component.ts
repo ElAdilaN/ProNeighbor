3d0c12b8fd91129c48d7487ecf9f4da6652e4dfc
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-23 21:32:58 +0100

    IMPLEMENTING -  AddParticipantComponent :   should load users on init - should toggle user selection - should not allow more than 3 users to be selected

--- ProNeighbor/src/Components/Chat/add-participant/add-participant.component.ts
+++ ProNeighbor/src/Components/Chat/add-participant/add-participant.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { UsersService } from '../../../services/users.service';
+import { User } from '../../../Model/user/user.model';
+import { ChatServiceService } from '../../../services/chat-service.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-participant',
@@ -7,4 +11,50 @@ import { Component } from '@angular/core';
   templateUrl: './add-participant.component.html',
   styleUrl: './add-participant.component.css',
 })
-export class AddParticipantComponent {}
+export class AddParticipantComponent {
+  users: User[] = [];
+  chatId: string | null = null;
+  selectedUserIds: string[] = [];
+  constructor(
+    private userService: UsersService,
+    private chatService: ChatServiceService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+  ngOnInit() {
+    this.chatId = this.activatedRoute.snapshot.paramMap.get('chatId');
+
+    this.loadAllUsers();
+  }
+  loadAllUsers() {
+    if (this.chatId) {
+      this.userService.getAllUsers(this.chatId).subscribe((data) => {
+        this.users = data;
+      });
+    }
+  }
+  toggleUserSelection(userId: string) {
+    const index = this.selectedUserIds.indexOf(userId);
+    if (index > -1) {
+      this.selectedUserIds.splice(index, 1);
+    } else {
+      if (this.selectedUserIds.length < 3) {
+        this.selectedUserIds.push(userId);
+      } else {
+        alert('You can select a maximum of 3 users.');
+      }
+    }
+  }
+
+  AddParticipant() {
+    for (let user of this.selectedUserIds) {
+      if (this.chatId) {
+        this.chatService.addParticipant(this.chatId, user).subscribe({
+          next: () => {},
+          error: (error) => {
+            console.error('Error fetching messages', error);
+          },
+        });
+      }
+    }
+  }
+}
