88631d00b366c609d7d8590dad6e13f39f4e6317
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-23 20:59:15 +0100

    TEST -  ServiceDetailsComponent - should load service details on init

--- ProNeighbor/src/Components/AppServices/service-details/service-details.component.spec.ts
+++ ProNeighbor/src/Components/AppServices/service-details/service-details.component.spec.ts
@@ -1,23 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ServiceDetailsComponent } from './service-details.component';
+import { ServicesService } from '../../../services/services.service';
+import { ActivatedRoute } from '@angular/router';
+import { of, throwError } from 'rxjs';
+import { Service } from '../../../Model/servicesProvider/service.model';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
 
 describe('ServiceDetailsComponent', () => {
   let component: ServiceDetailsComponent;
   let fixture: ComponentFixture<ServiceDetailsComponent>;
+  let servicesService: ServicesService;
+  let mockActivatedRoute: any;
 
   beforeEach(async () => {
+    mockActivatedRoute = {
+      paramMap: of({ get: (key: string) => '123' }), // Simulate route params
+    };
+
     await TestBed.configureTestingModule({
-      imports: [ServiceDetailsComponent]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule],
+      providers: [
+        ServicesService,
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ServiceDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    servicesService = TestBed.inject(ServicesService);
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load service details on init', () => {
+    const mockService: Service = new Service(
+      '123',
+      'Test Service',
+      50,
+      'Fixing Pipes',
+      'New York',
+      new Date(),
+      'Plumbing'
+    );
+
+    spyOn(servicesService, 'getServiceById').and.returnValue(of(mockService));
+
+    component.ngOnInit();
+    fixture.detectChanges();
+    fixture.whenStable();
+    expect(servicesService.getServiceById).toHaveBeenCalledWith('123');
+    expect(component.isLoading).toBeFalse();
   });
 });
--- ProNeighbor/src/Components/AppServices/service-details/service-details.component.ts
+++ ProNeighbor/src/Components/AppServices/service-details/service-details.component.ts
@@ -1,38 +1,13 @@
 import { Component } from '@angular/core';
-import { Service } from '../../../Model/servicesProvider/service.model';
-import { ActivatedRoute } from '@angular/router';
-import { ServicesService } from '../../../services/services.service';
+import { ReviewListComponent } from '../../Reviews/review-list/review-list.component';
 
 @Component({
   selector: 'app-service-details',
   standalone: true,
-  imports: [],
+  imports: [ReviewListComponent],
   templateUrl: './service-details.component.html',
   styleUrl: './service-details.component.css',
 })
 export class ServiceDetailsComponent {
-  serviceId: string | null = null;
-  service: Service | null = null;
 
-  constructor(
-    private route: ActivatedRoute,
-    private servicesService: ServicesService
-  ) {}
-
-  ngOnInit(): void {
-    // Extract the ID from the route
-    this.route.paramMap.subscribe((params) => {
-      this.serviceId = params.get('id');
-      if (this.serviceId) {
-        // this.loadServiceDetails(this.serviceId);
-      }
-    });
-  }
-
-  /*   loadServiceDetails(id: string) {
-    this.servicesService.getServiceById(id).subscribe({
-      next: (data) => (this.service = data),
-      error: (err) => console.error('Error loading service:', err),
-    });
-  } */
 }
