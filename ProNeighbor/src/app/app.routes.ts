8ec022a8e7882a104532f57d4e3e3986ef975124
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-23 22:59:30 +0100

    Correct the routes to show the chat within the dashboard

--- ProNeighbor/src/app/app.routes.ts
+++ ProNeighbor/src/app/app.routes.ts
@@ -5,63 +5,90 @@ import { ROLS } from '../Model/user/enum';
 import { UnauthorizedComponent } from '../Components/unauthorized/unauthorized.component';
 import { UserAreaComponent } from '../Components/user-area/user-area.component';
 import { ProviderAreaComponent } from '../Components/provider-area/provider-area.component';
-import { UserProfileComponent } from '../Components/Forms/Profiles/user-profile/user-profile.component';
+import { UserProfileComponent } from '../Components/user-profile/user-profile.component';
 import { ServiceComponent } from '../Components/AppServices/serviceRegister/service.component';
 import { ServiceListComponent } from '../Components/AppServices/service-list/service-list.component';
 import { LoginComponent } from '../Components/Auth/login/login.component';
 import { UserRegistrationComponent } from '../Components/Auth/user-registration/user-registration.component';
 import { ServiceDetailsComponent } from '../Components/AppServices/service-details/service-details.component';
-
+import { ReviewListComponent } from '../Components/Reviews/review-list/review-list.component';
+import { ReviewFormComponent } from '../Components/Reviews/review-form/review-form.component';
+import { DashboardComponent } from '../Components/dashboard/dashboard.component';
+import { ChatDashboardComponent } from '../Components/Chat/chat-dashboard/chat-dashboard.component';
+import { ChatListComponent } from '../Components/Chat/chat-list/chat-list.component';
+import { ChatWindowComponent } from '../Components/Chat/chat-window/chat-window.component';
+import { ChatInfoComponent } from '../Components/Chat/chat-info/chat-info.component';
+import { AddParticipantComponent } from '../Components/Chat/add-participant/add-participant.component';
 export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: UserRegistrationComponent },
+
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
-  },
-  {
-    path: 'register',
-    component: UserRegistrationComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
+    path: 'dashboard',
+    component: DashboardComponent,
     canActivate: [authGuard],
     data: { roles: [ROLS.USER, ROLS.PROVIDER] },
-  },
-  {
-    path: 'unauthorized',
-    component: UnauthorizedComponent,
-    canActivate: [authGuard],
-    data: { roles: [ROLS.USER, ROLS.PROVIDER] },
-  },
-  {
-    path: 'userArea',
-    component: UserAreaComponent,
-    canActivate: [authGuard],
-    data: { roles: [ROLS.USER] },
-  },
-  {
-    path: 'providerArea',
-    component: ProviderAreaComponent,
-    canActivate: [authGuard],
-    data: { roles: [ROLS.PROVIDER] },
-  },
-  {
-    path: 'UserProfile',
-    component: UserProfileComponent,
-  },
-  {
-    path: 'CreateService',
-    component: ServiceComponent,
-  },
-  {
-    path: 'ServiceList',
-    component: ServiceListComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'UserProfile', component: UserProfileComponent },
+      { path: 'ServiceList/:id', component: ServiceListComponent },
+      { path: 'ServiceDetail/:id', component: ServiceDetailsComponent },
+      {
+        path: 'ReviewsList/:id',
+        component: ReviewListComponent,
+      },
+      {
+        path: 'chat',
+        component: ChatDashboardComponent,
+        canActivate: [authGuard],
+        data: { roles: [ROLS.USER, ROLS.PROVIDER] },
+        children: [
+          { path: ':chatId', component: ChatWindowComponent },
+          { path: ':chatId/info', component: ChatInfoComponent },
+          {
+            path: ':chatId/add-participant',
+            component: AddParticipantComponent,
+          },
+        ],
+      },
+      // User-specific routes
+      {
+        path: 'userArea',
+        component: UserAreaComponent,
+        canActivate: [authGuard],
+        data: { roles: [ROLS.USER] },
+      },
+      {
+        path: 'ReviewForm',
+        component: ReviewFormComponent,
+        canActivate: [authGuard],
+        data: { roles: [ROLS.USER] },
+      },
+
+      // Provider-specific routes
+      {
+        path: 'providerArea',
+        component: ProviderAreaComponent,
+        canActivate: [authGuard],
+        data: { roles: [ROLS.PROVIDER] },
+      },
+      {
+        path: 'CreateService',
+        component: ServiceComponent,
+        canActivate: [authGuard],
+        data: { roles: [ROLS.PROVIDER] },
+      }, //service Id
+
+      {
+        path: 'ReviewForm',
+        component: ReviewFormComponent,
+        canActivate: [authGuard],
+        data: { roles: [ROLS.USER] },
+      },
+    ],
   },
 
-  { path: 'ServiceList/:id', component: ServiceDetailsComponent }, // Service details page
+  { path: 'unauthorized', component: UnauthorizedComponent },
+  // { path: '**', redirectTo: 'login' },
 ];
