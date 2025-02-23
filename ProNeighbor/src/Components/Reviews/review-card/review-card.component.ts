955f16484629bbb42831be341548268512f4e4c9
Noureddin El Adila <noureddineladila@gmail.com>
2025-02-23 23:42:01 +0100

    IMPLEMENTING - ReviewCardComponent :   should display the review information correctly - should display rating stars based on the rating value -  should call editReview when the edit button is clicked - should call deleteReview when the delete button is clicked

--- ProNeighbor/src/Components/Reviews/review-card/review-card.component.ts
+++ ProNeighbor/src/Components/Reviews/review-card/review-card.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ReviewDTO } from '../../../Model/appServices/review.model';
+import { AuthService } from '../../../services/auth.service';
+import { ReviewsService } from '../../../services/reviews.service';
 
 @Component({
   selector: 'app-review-card',
   standalone: true,
   imports: [],
   templateUrl: './review-card.component.html',
-  styleUrl: './review-card.component.css'
+  styleUrl: './review-card.component.css',
 })
 export class ReviewCardComponent {
+  @Input() review!: ReviewDTO;
+  @Output() reviewDeleted = new EventEmitter<void>();
 
+  loggedInUserId!: string | null;
+
+  constructor(
+    private authService: AuthService,
+    private reviewService: ReviewsService
+  ) {}
+
+  ngOnInit() {
+    this.loggedInUserId = this.authService.getUserIdFromToken(); // Get user ID from token
+  }
+
+  editReview(review: ReviewDTO) {
+    console.log('Edit review:', review);
+  }
+
+  deleteReview(reviewId: string) {
+    if (confirm('Are you sure you want to delete this review?')) {
+      this.reviewService.deleteReview(reviewId).subscribe({
+        next: () => {
+          console.log('Review deleted successfully');
+          this.reviewDeleted.emit(); // Emit the event after successful deletion
+        },
+        error: (err) => console.error('Error deleting review:', err),
+      });
+    }
+  }
 }
