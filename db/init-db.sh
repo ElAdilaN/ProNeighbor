2704672f27dedcdb0c4a2e353cb4051517c8f127
Noureddin El Adila <noureddineladila@gmail.com>
2025-03-07 21:38:24 +0100

    Docker configuration

--- db/init-db.sh
+++ db/init-db.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Waiting for SQL Server to start..."
-sleep 20s  # Wait for SQL Server to initialize
+sleep 30s  # Ensure the database engine is up
 
 echo "Running database initialization script..."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P StrongP@ssword123 -d master -i /docker-entrypoint-initdb.d/FinalProNeighbor.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -d master -i /init-db.sql
 
 echo "Database setup complete!"
+tail -f /dev/null  # Keep the container running
